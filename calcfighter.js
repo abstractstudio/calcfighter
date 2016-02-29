39c7e6fc4a677e7f3713f0be98970f4cdd65bf52
Arman <arman.siddique887@gmail.com>
2016-02-28 21:39:06 -0500

    Worked on jump code
    Not working yet.

--- calcfighter.js
+++ calcfighter.js
@@ -12,7 +12,11 @@ var platforms;
 
 /* Constants. */
 var SPEED = 0.5;
-var ACCELERATION = 0.25;
+var GRAVITY = 0.45;
+
+var JUMP = 10;
+var MAX_JUMPS = 2;
+var JUMP_COOLDOWN = 400;
 
 /* Animation. */
 var w = window;
@@ -41,8 +45,8 @@ function setup() {
     /* Configure the canvas. */
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
-    canvas.width = 700;
-    canvas.height = 300;
+    canvas.width = 1280;
+    canvas.height = 1024;
     document.body.appendChild(canvas);
     
     /* Set up tick rate. */
@@ -60,7 +64,7 @@ function setup() {
     
     /* Platforms. */
     platforms = [
-        {x: 50, y: 250, width: 600, height: 4}
+        [(canvas.width - 600)/2, canvas.height * 13/20, 600, 4]
     ];
     
     /* Start the game. */
@@ -73,58 +77,132 @@ function reset() {
     
     /* Set the contents of each player. */
     zero = {
-        x: 100, 
+        x: 400, 
         y: 50,
         yv: 0,
-        grounded: true, 
+        grounded: false, 
+		jumping: 0, 
+		jumpTime: 0, 
         character: "0",
-        bbox: function() { return [zero.x+1, zero.y+1, 21, -29]; }
+        bbox: function() { return [zero.x+1, zero.y+1, 21, 29]; }
     };
     
     infinitus = {
-        x: 600, 
+        x: 800, 
         y: 50,
         yv: 0,
-        grounded: true, 
+        grounded: false, 
+		jumping: 0, 
+		jumpTime: 0, 
         character: "\u221E",
-        bbox: function() { return [infinitus.x+2, infinitus.y-2, 32, -19]; }
+        bbox: function() { return [infinitus.x+2, infinitus.y+7, 33, 19]; }
     };
     
 }
 
 function update(delta) {
     
-    /* Player movement. */
+    /* Horizontal player movement. */
     if (65 in keys) zero.x -= SPEED * delta; // a
     if (68 in keys) zero.x += SPEED * delta; // d
     if (37 in keys) infinitus.x -= SPEED * delta; // left
     if (39 in keys) infinitus.x += SPEED * delta; // right
     
+	/* Vertical player movement. */
+	if (zero.jumping < MAX_JUMPS && Date.now() - zero.jumpTime > JUMP_COOLDOWN && 87 in keys) {
+		zero.grounded = false;
+		zero.jumping++;
+		zero.jumpTime = Date.now();
+		zero.yv = -JUMP;
+	}
+	if (!zero.grounded) {
+		zero.yv += GRAVITY;
+	} else {
+		zero.yv = 0;
+		zero.jumping = 0;
+	}
+	zero.y += zero.yv;
+	
+	if (!infinitus.grounded) {
+		infinitus.yv += GRAVITY;
+		infinitus.y += infinitus.yv;
+	} else {
+		infinitus.yv = 0;
+	}
+	
+	/* Check if players are off screen. */
+	zeroBBox = zero.bbox();
+	infinBBox = infinitus.bbox();
+	
+	for (var i = 0; i < platforms.length; i++) {
+		if (zero.yv > 0 && rectIntersect(zeroBBox, platforms[i])) {
+			// Make sure the player doesn't go through the platform
+			if (zero.y <= platforms[i][1]) zero.y = platforms[i][1] - zeroBBox[3];
+			console.log("grounding");
+			zero.yv = 0;
+			zero.grounded = true;
+			zero.jumping = 0;
+			zero.jumpTime = Date.now();
+			//alert("GROUDNEDEDEDED")
+		} else {
+			zero.grounded = false;
+		}
+		
+		if (rectIntersect(infinBBox, platforms[i])) {
+			if (infinitus.y <= platforms[i][1]) infinitus.y = platforms[i][1] - infinBBox[3];
+			else infinitus.y = platforms[i][1];
+			
+			infinitus.grounded = true;
+		} else {
+			infinitus.grounded = false;
+		}
+	}
+	
+	if (zero.x < 0) zero.x = 0;
+	else if (zero.x + zeroBBox[2] >= canvas.width) zero.x = canvas.width - zeroBBox[2];
+	if (zero.y < 0) zero.y = 0;
+	else if (zero.y + zeroBBox[3] >= canvas.height) {
+		//zero.y = canvas.height - zeroBBox[3];
+		respawn(zero);
+	}
+	
+	if (infinitus.x < 0) infinitus.x = 0;
+	else if (infinitus.x + infinBBox[2] >= canvas.width) infinitus.x = canvas.width - infinBBox[2];
+	if (infinitus.y < 0) infinitus.y = 0;
+	else if (infinitus.y + infinBBox[3] >= canvas.height) infinitus.y = canvas.height - infinBBox[3];
+	
     /* Collisions. */
-    
-    
+}
+
+function respawn(player) {
+	player.y = 0;
+	player.yv = 0;
+}
+
+function rectIntersect(r1, r2) {
+	return !(r1[0]+r1[2] < r2[0] || r1[0] > r2[0]+r2[2] || r1[1]+r1[3] < r2[1] || r1[1] > r2[1]+r2[3]);
 }
 
 function render() {
-    
     /* Redraw the background. */
-    context.fillStyle = "white";
+    context.fillStyle = "#CCCCCC";
     context.fillRect(0, 0, canvas.width, canvas.height);
     
     /* Draw the platforms. */
     context.fillStyle = "black";
     for (var i = 0; i < platforms.length; i++) {
-        context.fillRect(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height);
+        context.fillRect(platforms[i][0], platforms[i][1], platforms[i][2], platforms[i][3]);
     }
     
     /* Draw the players. */
+	context.textBaseline = "hanging";
     context.fillStyle = "black";
     context.font = "36px Verdana";
-    //context.strokeRect(zero.x+1, zero.y+1, 21, -29);
-    z = zero.bbox();
-    context.strokeRect(z[0], z[1], z[2], z[3]);
+    bbox = zero.bbox();
+	context.strokeRect(bbox[0], bbox[1], bbox[2], bbox[3]);
     context.fillText(zero.character, zero.x, zero.y);
-    context.strokeRect(infinitus.x+2, infinitus.y-2, 32, -19);
+	bbox = infinitus.bbox();
+	context.strokeRect(bbox[0], bbox[1], bbox[2], bbox[3]);
     context.fillText(infinitus.character, infinitus.x, infinitus.y);
     
 }
