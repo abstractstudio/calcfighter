3d998a0f9ce30379bd97b3819c9853db9d847f39
Noah Kim <noahbkim@gmail.com>
2016-02-28 22:59:58 -0500

    The bbox and cbox are returning NaN
    For some reason the bbox() and cbox() methods of the players are
    returning NaN instead of the actual value, which is weird because it
    wasn't happening before.

--- calcfighter.js
+++ calcfighter.js
@@ -8,20 +8,32 @@ var keys;
 /* Players and map. */
 var zero;
 var infinitus;
+var players = {"zero": zero, "infinitus": infinitus};
 var platforms;
 
 /* Constants. */
-var SPEED = 0.5;
-var GRAVITY = 0.45;
-
+var XV_ACCELERATION = 0.01;
+var XV_TERMINAL = 0.1;
+var XV_FRICTION = 0.1;
+var YV_GRAVITY = 0.45;
+var YV_TERMINAL = 0.5;
 var JUMP = 10;
-var MAX_JUMPS = 2;
+var JUMP_MAX = 2;
 var JUMP_COOLDOWN = 400;
+
+var SNAP_TO_EDGE = 2;
+
+/* Useful. */
+var X = 0; var Y = 1;
+var W = 2; var H = 3;
+var X1 = 0; var X2 = 2;
+var Y1 = 1; var Y2 = 3;
 
 /* Animation. */
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
+/* Game library. */
 function main() {
     
     /* Record the initial tick time. */
@@ -45,8 +57,8 @@ function setup() {
     /* Configure the canvas. */
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
-    canvas.width = 1280;
-    canvas.height = 1024;
+    canvas.width = 854;
+    canvas.height = 480;
     document.body.appendChild(canvas);
     
     /* Set up tick rate. */
@@ -77,109 +89,144 @@ function reset() {
     
     /* Set the contents of each player. */
     zero = {
+        character: "0",
+        name: "zero",
+
         x: 400, 
         y: 50,
+        xv: 0,
         yv: 0,
+        
         grounded: false, 
-		jumping: 0, 
+		jumpState: 0, 
 		jumpTime: 0, 
-        character: "0",
-        bbox: function() { return [zero.x+1, zero.y+1, 21, 29]; }
+        
+        score: 0,
+        
+        bbox: function() { return [zero.x+1, zero.y+1, 21, 29]; },
+        cbox: function() { return [zero.x+1, zero.y+1, zero.x+21, zero.y+29]; },
+        
+        leftKey: 65,
+        rightKey: 68,
+        upKey: 87,
+        downKey: 83,
     };
     
     infinitus = {
+        character: "\u221E",
+        name: "infinitus",
+
         x: 800, 
         y: 50,
+        xv: 0,
         yv: 0,
+        
         grounded: false, 
-		jumping: 0, 
+		jumpState: 0, 
 		jumpTime: 0, 
-        character: "\u221E",
-        bbox: function() { return [infinitus.x+2, infinitus.y+7, 33, 19]; }
+        
+        score: 0,
+        
+        bbox: function() { return [infinitus.x+2, infinitus.y+7, 33, 19]; },
+        cbox: function() { return [infinitus.x+2, infinitus.y+7, infinitus.x+33, infinitus.y+19]; },
+        
+        leftKey: 37,
+        rightKey: 39,
+        upKey: 38,
+        downKey: 40,
     };
+    
+    players["zero"] = zero;
+    players["infinitus"] = infinitus;
     
 }
 
 function update(delta) {
     
-    /* Horizontal player movement. */
-    if (65 in keys) zero.x -= SPEED * delta; // a
-    if (68 in keys) zero.x += SPEED * delta; // d
-    if (37 in keys) infinitus.x -= SPEED * delta; // left
-    if (39 in keys) infinitus.x += SPEED * delta; // right
+    /* Update the players individually. */
+    for (var name in players) {
+        
+        /* Obtain the player. */
+        var player = players[name];
+                
+        /* Strafing. */
+        if (player.leftKey in keys) player.xv -= XV_ACCELERATION;
+        if (player.rightKey in keys) player.xv += XV_ACCELERATION;
+        if (Math.abs(player.xv) > XV_TERMINAL) player.xv = (player.xv > 0 ? 1 : -1) * XV_TERMINAL;
+        
+        /* Jumping. */
+        if (player.jumpState < JUMP_MAX && Date.now() - player.jumpTime > JUMP_COOLDOWN && player.upKey in keys) {
+            player.grounded = false;
+            player.jumpState++;
+            player.jumpTime = Date.now();
+            player.yv = -JUMP;
+        }
+        
+        if (!zero.grounded) {
+            player.yv += YV_GRAVITY;
+        } else {
+            player.yv = 0;
+            player.jumpState = 0;
+        }
+        
+        if (Math.abs(player.yv) > YV_TERMINAL) (player.yv > 0 ? 1 : -1) * YV_TERMINAL;
+        
+        /* Actually move. */
+        console.log(player.x, player.y, player.xv, player.yv);
+        player.x += player.xv * delta;
+        player.y += player.yv * delta;
+        
+        /* Collisions. */
+        var bbox = player.bbox();
+        var cbox = player.cbox();
+        
+        console.log(X1);
+        console.log(bbox);
+        
+        for (var platform in platforms) {
+            
+            /* Check if colliding with platform. */
+            if (player.yv > 0 && intersects(bbox, platform)) {
+                
+                /* Check if close enough to snap up to platform. */
+                if (Math.abs(platform[Y] - cbox[Y2]) < SNAP_TO_EDGE) player.y = platform[Y] - bbox[Y2];
+                player.yv = 0;
+                player.grounded = true;
+                player.jumpState = 0;
+                
+            } else {
+                
+                /* Otherwise, the player is ungrounded. */
+                player.grounded = false;
+                
+            }
+        }
+        
+        /* Edge. */
+        if (cbox[X1] < 0) player.x = 0;
+        else if (cbox[X2] > canvas.width) player.x = canvas.width - bbox[W];
+        if (cbox[Y1] < 0) player.y = 0;
+        else if (player.y + bbox[H] > canvas.height) die(player);
+        
+    }
     
-	/* Vertical player movement. */
-	if (zero.jumping < MAX_JUMPS && Date.now() - zero.jumpTime > JUMP_COOLDOWN && 87 in keys) {
-		zero.grounded = false;
-		zero.jumping++;
-		zero.jumpTime = Date.now();
-		zero.yv = -JUMP;
-	}
-	if (!zero.grounded) {
-		zero.yv += GRAVITY;
-	} else {
-		zero.yv = 0;
-		zero.jumping = 0;
-	}
-	zero.y += zero.yv;
-	
-	if (!infinitus.grounded) {
-		infinitus.yv += GRAVITY;
-		infinitus.y += infinitus.yv;
-	} else {
-		infinitus.yv = 0;
-	}
-	
-	/* Check if players are off screen. */
-	zeroBBox = zero.bbox();
-	infinBBox = infinitus.bbox();
-	
-	for (var i = 0; i < platforms.length; i++) {
-		if (zero.yv > 0 && rectIntersect(zeroBBox, platforms[i])) {
-			// Make sure the player doesn't go through the platform
-			if (zero.y <= platforms[i][1]) zero.y = platforms[i][1] - zeroBBox[3];
-			console.log("grounding");
-			zero.yv = 0;
-			zero.grounded = true;
-			zero.jumping = 0;
-			zero.jumpTime = Date.now();
-			//alert("GROUDNEDEDEDED")
-		} else {
-			zero.grounded = false;
-		}
-		
-		if (rectIntersect(infinBBox, platforms[i])) {
-			if (infinitus.y <= platforms[i][1]) infinitus.y = platforms[i][1] - infinBBox[3];
-			else infinitus.y = platforms[i][1];
-			
-			infinitus.grounded = true;
-		} else {
-			infinitus.grounded = false;
-		}
-	}
-	
-	if (zero.x < 0) zero.x = 0;
-	else if (zero.x + zeroBBox[2] >= canvas.width) zero.x = canvas.width - zeroBBox[2];
-	if (zero.y < 0) zero.y = 0;
-	else if (zero.y + zeroBBox[3] >= canvas.height) {
-		//zero.y = canvas.height - zeroBBox[3];
-		respawn(zero);
-	}
-	
-	if (infinitus.x < 0) infinitus.x = 0;
-	else if (infinitus.x + infinBBox[2] >= canvas.width) infinitus.x = canvas.width - infinBBox[2];
-	if (infinitus.y < 0) infinitus.y = 0;
-	else if (infinitus.y + infinBBox[3] >= canvas.height) infinitus.y = canvas.height - infinBBox[3];
-	
-    /* Collisions. */
 }
 
+function die(player) {
+    if (player.name == "zero") infinitus.score++;
+    else if (player.name == "infinitus") zero.score++;
+    player.y = 0;
+    player.yv = 0;
+    console.log("DIE");
+}
+    
 function respawn(player) {
+    console.log("RESPAWN");
 	player.y = 0;
 	player.yv = 0;
 }
 
-function rectIntersect(r1, r2) {
+function intersects(r1, r2) {
 	return !(r1[0]+r1[2] < r2[0] || r1[0] > r2[0]+r2[2] || r1[1]+r1[3] < r2[1] || r1[1] > r2[1]+r2[3]);
 }
 
